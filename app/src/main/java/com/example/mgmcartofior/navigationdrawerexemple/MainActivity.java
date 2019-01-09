9f1e1dc48b0d54f42cd257bfee21d98cc58dcb0b
alexutu100 <mgm.cartofior@yahoo.ro>
2019-01-09 19:23:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mgmcartofior/navigationdrawerexemple/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.mgmcartofior.navigationdrawerexemple;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    private void initView() {
+        drawer = findViewById(R.id.drawer_layout);
+        textView = findViewById(R.id.text_view);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_message) {
+            Toast.makeText(getApplicationContext(), "Ati facut click pe " + item.getTitle(), Toast.LENGTH_LONG).show();
+            drawer.closeDrawers();
+            textView.setText(item.getTitle());
+        }
+        if (id == R.id.camera) {
+            Toast.makeText(getApplicationContext(), "Ati facut click pe " + item.getTitle(), Toast.LENGTH_LONG).show();
+            drawer.closeDrawers();
+            textView.setText(item.getTitle());
+        }
+        if (id == R.id.gallery) {
+            Toast.makeText(getApplicationContext(), "Ati facut click pe " + item.getTitle(), Toast.LENGTH_LONG).show();
+            drawer.closeDrawers();
+            textView.setText(item.getTitle());
+        }
+        if (id == R.id.send) {
+            Toast.makeText(getApplicationContext(), "Ati facut click pe " + item.getTitle(), Toast.LENGTH_LONG).show();
+            drawer.closeDrawers();
+            textView.setText(item.getTitle());
+        }
+        if (id == R.id.slide_show) {
+            Toast.makeText(getApplicationContext(), "Ati facut click pe " + item.getTitle(), Toast.LENGTH_LONG).show();
+            drawer.closeDrawers();
+            textView.setText(item.getTitle());
+        }
+        if (id == R.id.share) {
+            Toast.makeText(getApplicationContext(), "Ati facut click pe " + item.getTitle(), Toast.LENGTH_LONG).show();
+            drawer.closeDrawers();
+            textView.setText(item.getTitle());
+        }
+        return true;
+    }
+}
